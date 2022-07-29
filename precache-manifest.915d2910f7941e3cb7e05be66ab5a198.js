1bee2069c08d80b2d0d303dd794f3dabc2f7806a
mehtaumesh1245 <mehtaumesh1245@gmail.com>
2022-07-29 16:25:52 +0545

    Updates

---
+++ precache-manifest.915d2910f7941e3cb7e05be66ab5a198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd143ae36722f29af407",
+    "url": "/TextUtils-React/static/css/main.4405351c.chunk.css"
+  },
+  {
+    "revision": "fd143ae36722f29af407",
+    "url": "/TextUtils-React/static/js/main.8596386c.chunk.js"
+  },
+  {
+    "revision": "9b371c23fd5207bcc783",
+    "url": "/TextUtils-React/static/js/runtime~main.7eedb0cd.js"
+  },
+  {
+    "revision": "d7552a1f1f052f900238",
+    "url": "/TextUtils-React/static/js/2.265c2a91.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/TextUtils-React/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "90fd30ba1b6287e084ee4f27a2a98f66",
+    "url": "/TextUtils-React/index.html"
+  }
+];
